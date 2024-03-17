2b3be77c8c4f0958332d29b7281f5d84d3e9d3ed
Edgars <klopojed.son@gmail.com>
2024-03-18 01:22:40 +0200

    Updated

--- calculate.c
+++ calculate.c
@@ -1,48 +1,51 @@
-// https://en.wikipedia.org/wiki/English_Qaballa
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+short int sum = 0;
+
+void add(char character) {
+	if (character >= 'a') {
+		character -= ' ';
+	}
+
+	switch(character) {
+		case 'A': sum +=  1; return;
+		case 'L': sum +=  2; return;
+		case 'W': sum +=  3; return;
+		case 'H': sum +=  4; return;
+		case 'S': sum +=  5; return;
+		case 'D': sum +=  6; return;
+		case 'O': sum +=  7; return;
+		case 'Z': sum +=  8; return;
+		case 'K': sum +=  9; return;
+		case 'V': sum += 10; return;
+		case 'G': sum += 11; return;
+		case 'R': sum += 12; return;
+		case 'C': sum += 13; return;
+		case 'N': sum += 14; return;
+		case 'Y': sum += 15; return;
+		case 'J': sum += 16; return;
+		case 'U': sum += 17; return;
+		case 'F': sum += 18; return;
+		case 'Q': sum += 19; return;
+		case 'B': sum += 20; return;
+		case 'M': sum += 21; return;
+		case 'X': sum += 22; return;
+		case 'I': sum += 23; return;
+		case 'T': sum += 24; return;
+		case 'E': sum += 25; return;
+		case 'P': sum += 26; return;
+	}
+}
+
+unsigned char main() {
 	char *line = 0;
-	short int sum = 0;
 	size_t length;
 
 	getline(&line, &length, stdin);
 
 	for (unsigned char i = 0; line[i] != '\n'; i++) {
-		if (line[i] >= 'a') {
-			line[i] -= ' ';
-		}
-
-		switch(line[i]) {
-			case 'A': sum +=  1; break;
-			case 'L': sum +=  2; break;
-			case 'W': sum +=  3; break;
-			case 'H': sum +=  4; break;
-			case 'S': sum +=  5; break;
-			case 'D': sum +=  6; break;
-			case 'O': sum +=  7; break;
-			case 'Z': sum +=  8; break;
-			case 'K': sum +=  9; break;
-			case 'V': sum += 10; break;
-			case 'G': sum += 11; break;
-			case 'R': sum += 12; break;
-			case 'C': sum += 13; break;
-			case 'N': sum += 14; break;
-			case 'Y': sum += 15; break;
-			case 'J': sum += 16; break;
-			case 'U': sum += 17; break;
-			case 'F': sum += 18; break;
-			case 'Q': sum += 19; break;
-			case 'B': sum += 20; break;
-			case 'M': sum += 21; break;
-			case 'X': sum += 22; break;
-			case 'I': sum += 23; break;
-			case 'T': sum += 24; break;
-			case 'E': sum += 25; break;
-			case 'P': sum += 26; break;
-		}
+		add(line[i]);
 	}
 
 	printf("%d\n", sum);
