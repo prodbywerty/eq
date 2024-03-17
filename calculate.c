afc3f30ff377f6e1f9de11e74e22ca275dee99a5
Edgars <klopojed.son@gmail.com>
2024-03-18 00:01:54 +0200

    Amazing!

--- calculate.c
+++ calculate.c
@@ -1,85 +1,47 @@
+// https://en.wikipedia.org/wiki/English_Qaballa
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main() {
 	char *line = 0;
-	unsigned char sum = 0;
+	short int sum = 0;
 	size_t length;
 
 	getline(&line, &length, stdin);
 
 	for (unsigned char i = 0; line[i] != '\n'; i++) {
+		if (line[i] >= 'a') {
+			line[i] -= ' ';
+		}
+
 		switch(line[i]) {
-			case 'a':
-			case 'j':
-			case 's':
-			case 'A':
-			case 'J':
-			case 'S':
-				sum += 1;
-				break;
-			case 'b':
-			case 'k':
-			case 't':
-			case 'B':
-			case 'K':
-			case 'T':
-				sum += 2;
-				break;
-			case 'c':
-			case 'l':
-			case 'u':
-			case 'C':
-			case 'L':
-			case 'U':
-				sum += 3;
-				break;
-			case 'd':
-			case 'm':
-			case 'v':
-			case 'D':
-			case 'M':
-			case 'V':
-				sum += 4;
-				break;
-			case 'e':
-			case 'n':
-			case 'w':
-			case 'E':
-			case 'N':
-			case 'W':
-				sum += 5;
-				break;
-			case 'f':
-			case 'o':
-			case 'x':
-			case 'F':
-			case 'O':
-			case 'X':
-				sum += 6;
-				break;
-			case 'g':
-			case 'p':
-			case 'y':
-			case 'G':
-			case 'P':
-			case 'Y':
-				sum += 7;
-				break;
-			case 'h':
-			case 'q':
-			case 'z':
-			case 'H':
-			case 'Q':
-			case 'Z':
-				sum += 8;
-				break;
-			case 'i':
-			case 'r':
-			case 'I':
-			case 'R':
-				sum += 9;
-				break;
+			case 'A': sum +=  1; break;
+			case 'L': sum +=  2; break;
+			case 'W': sum +=  3; break;
+			case 'H': sum +=  4; break;
+			case 'S': sum +=  5; break;
+			case 'D': sum +=  6; break;
+			case 'O': sum +=  7; break;
+			case 'Z': sum +=  8; break;
+			case 'K': sum +=  9; break;
+			case 'V': sum += 10; break;
+			case 'G': sum += 11; break;
+			case 'R': sum += 12; break;
+			case 'C': sum += 13; break;
+			case 'N': sum += 14; break;
+			case 'Y': sum += 15; break;
+			case 'J': sum += 16; break;
+			case 'U': sum += 17; break;
+			case 'F': sum += 18; break;
+			case 'Q': sum += 19; break;
+			case 'B': sum += 20; break;
+			case 'M': sum += 21; break;
+			case 'X': sum += 22; break;
+			case 'I': sum += 23; break;
+			case 'T': sum += 24; break;
+			case 'E': sum += 25; break;
+			case 'P': sum += 26; break;
 		}
 	}
 
