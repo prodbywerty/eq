760269f949d601eb4780649571328f7c01cdbdab
Edgars <klopojed.son@gmail.com>
2024-04-12 17:56:41 +0300

    Re-written

--- calculate.c
+++ calculate.c
@@ -1,54 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-short int sum = 0;
+int main() {
+	int sum = 0;
 
-void add(char character) {
-	if (character >= 'a') {
-		character -= ' ';
+	top:
+	switch (getchar()) {
+		default: goto top;
+		case 'A': case 'a': sum +=  1; goto top;
+		case 'L': case 'l': sum +=  2; goto top;
+		case 'W': case 'w': sum +=  3; goto top;
+		case 'H': case 'h': sum +=  4; goto top;
+		case 'S': case 's': sum +=  5; goto top;
+		case 'D': case 'd': sum +=  6; goto top;
+		case 'O': case 'o': sum +=  7; goto top;
+		case 'Z': case 'z': sum +=  8; goto top;
+		case 'K': case 'k': sum +=  9; goto top;
+		case 'V': case 'v': sum += 10; goto top;
+		case 'G': case 'g': sum += 11; goto top;
+		case 'R': case 'r': sum += 12; goto top;
+		case 'C': case 'c': sum += 13; goto top;
+		case 'N': case 'n': sum += 14; goto top;
+		case 'Y': case 'y': sum += 15; goto top;
+		case 'J': case 'j': sum += 16; goto top;
+		case 'U': case 'u': sum += 17; goto top;
+		case 'F': case 'f': sum += 18; goto top;
+		case 'Q': case 'q': sum += 19; goto top;
+		case 'B': case 'b': sum += 20; goto top;
+		case 'M': case 'm': sum += 21; goto top;
+		case 'X': case 'x': sum += 22; goto top;
+		case 'I': case 'i': sum += 23; goto top;
+		case 'T': case 't': sum += 24; goto top;
+		case 'E': case 'e': sum += 25; goto top;
+		case 'P': case 'p': sum += 26; goto top;
+		case '\n': printf("%d\n", sum);
 	}
-
-	switch(character) {
-		case 'A': sum +=  1; return;
-		case 'L': sum +=  2; return;
-		case 'W': sum +=  3; return;
-		case 'H': sum +=  4; return;
-		case 'S': sum +=  5; return;
-		case 'D': sum +=  6; return;
-		case 'O': sum +=  7; return;
-		case 'Z': sum +=  8; return;
-		case 'K': sum +=  9; return;
-		case 'V': sum += 10; return;
-		case 'G': sum += 11; return;
-		case 'R': sum += 12; return;
-		case 'C': sum += 13; return;
-		case 'N': sum += 14; return;
-		case 'Y': sum += 15; return;
-		case 'J': sum += 16; return;
-		case 'U': sum += 17; return;
-		case 'F': sum += 18; return;
-		case 'Q': sum += 19; return;
-		case 'B': sum += 20; return;
-		case 'M': sum += 21; return;
-		case 'X': sum += 22; return;
-		case 'I': sum += 23; return;
-		case 'T': sum += 24; return;
-		case 'E': sum += 25; return;
-		case 'P': sum += 26; return;
-	}
-}
-
-unsigned char main() {
-	char *line = 0;
-	size_t length;
-
-	getline(&line, &length, stdin);
-
-	for (unsigned char i = 0; line[i] != '\n'; i++) {
-		add(line[i]);
-	}
-
-	printf("%d\n", sum);
-
-	free(line);
 }
